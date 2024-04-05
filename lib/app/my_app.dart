06d4f5e8bc83588aa02df441142dfa66c697816b
Volha Smirnova <olga@voccent.com>
2024-04-05 12:57:13 +0200

    #1 create structure

---
+++ lib/app/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:therapist/presentation/screens/choose_therapist_screen/choose_therapist_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Therapist',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ChooseTherapistPage(),
+    );
+  }
+}
